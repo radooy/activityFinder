5cb8c1024bb7c937222a98b2280b24102484b1ac
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-19 18:52:41 +0200

    Implement cookie adding after successful login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -22,7 +22,8 @@ router.post("/login", (req,res)=>{
     let {username, password} = req.body;
     authService.login(username,password)    
         .then(({token, _id, username, city})=>{
-            res.status(200).json({token, username, _id, city});
+            res.cookie("AuthCookie", token,{httpOnly:true});
+            res.status(200).json({token, username, _id, city}); //if needed for client localStorage
         })
         .catch(err=>{
             console.log(err.message);
