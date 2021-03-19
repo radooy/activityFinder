977ed2e893d3eced1ca83f5129b6fdc28a8ee107
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-19 19:09:49 +0200

    Updated auth

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 const { SECRET } = require("../config/config")
 
 function auth(req, res, next) {
+
+    //With jwt and try-catch token stored in clients localStorage
     let authHeader = req.get('Authorization');
     if (authHeader) {
         let token = authHeader.split(' ')[1];
@@ -11,13 +13,27 @@ function auth(req, res, next) {
         } catch (err) {
             return next(); //error possible -  you are not authorized, fake token
         }
+    }
+    
+    //With cookie and callback
+    // let tokenFromCookie = req.cookies["AuthCookie"];
+
+    // if (tokenFromCookie) {
+    //     jwt.verify(tokenFromCookie, SECRET, function(err,decoded){
+    //         if (err) {
+    //             res.clearCookie("AuthCookie");
+    //         }else{
+    //             req.user = decoded;
+    //         }
+    //     });
+    // }
 
         //dummy code for backend postman test
-        
+
         // req.user = {
         //     id:"60548fce0f44872b483a824c",
         // }
-    }
+    
     next();
 };
 
