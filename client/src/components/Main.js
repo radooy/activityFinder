2f8257ab177adbc3589fde72d615260254b35bab
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-22 23:30:38 +0200

    Installed react-datetime-picker
    Installed and done some practice with react-datetime-picker

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,7 +1,18 @@
+import React, { useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
+
 const Main = () => {
-    return(
-        <h1>Activity Finder Project</h1>
-    )
+    const [value, onChange] = useState(new Date());
+    console.log(value);
+
+  return (
+    <div>
+      <DateTimePicker
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
 }
 
 export default Main;
