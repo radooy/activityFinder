0c55c010a1617207efd3dbb0b62cef2793f3520c
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-23 15:16:04 +0200

    Removed demo code

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,27 +5,11 @@ import {StyledHeader, StyledUl, StyledLi, Logo } from "./headerStyle";
 class Header extends React.Component{
     constructor(props){
         super(props)
-        // this.state = {
-        //     cities: [
-
-        //     ]
-        // }
+        
     }
     
-    //Test if data is accessible from restapi
-    // componentDidMount() {
-    //     fetch("http://localhost:5000/api/utils/cities")
-    //     .then(res => res.json())
-    //     .then((data) => {
-    //       this.setState({ cities: data.cities })
-    //       console.log(data.cities);
-    //     })
-    //     .catch(err=>console.log(err));
-    //   }
-
     render(){
         return (
-            /* Will be transformed to Link To */
             <StyledHeader>
                 <Link to="/" className = "logo"><Logo src="https://i.postimg.cc/65Jr9g81/dark-logo-transparent.png" alt="Activity-Finder"/></Link>
                 <StyledUl>
