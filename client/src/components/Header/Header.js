58a664c518fac47147e5902b9c5811a2fce53ac2
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-23 16:46:20 +0200

    Reworked header to a functional component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,24 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {StyledHeader, StyledUl, StyledLi, Logo } from "./headerStyle";
+import { StyledHeader, StyledUl, StyledLi, Logo } from "./headerStyle";
 
-class Header extends React.Component{
-    constructor(props){
-        super(props)
-        
-    }
-    
-    render(){
-        return (
-            <StyledHeader>
-                <Link to="/" className = "logo"><Logo src="https://i.postimg.cc/65Jr9g81/dark-logo-transparent.png" alt="Activity-Finder"/></Link>
-                <StyledUl>
-                    <Link to="/register" className="auth-link"> <StyledLi>Register</StyledLi> </Link>
-                    <Link to="/login" className="auth-link"> <StyledLi>Login</StyledLi> </Link>
-                </StyledUl>
-            </StyledHeader>
-        );
-    };
-}
+const Header = () => {
+    return (
+        <StyledHeader>
+            <Link to="/" className="logo"><Logo src="https://i.postimg.cc/65Jr9g81/dark-logo-transparent.png" alt="Activity-Finder" /></Link>
+            <StyledUl>
+                <Link to="/register" className="auth-link"> <StyledLi>Register</StyledLi> </Link>
+                <Link to="/login" className="auth-link"> <StyledLi>Login</StyledLi> </Link>
+            </StyledUl>
+        </StyledHeader>
+    );
+};
 
 export default Header;
