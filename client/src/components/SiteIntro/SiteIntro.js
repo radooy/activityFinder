f35c4f8f5d878fa7093295629f3712cecf4d3c3b
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-23 15:14:21 +0200

    Update SiteIntro.js

--- client/src/components/SiteIntro/SiteIntro.js
+++ client/src/components/SiteIntro/SiteIntro.js
@@ -1,12 +1,12 @@
-import { Heading, Paragraph } from "./siteIntroStyle";
+import { IntroContainer, Heading, Paragraph } from "./siteIntroStyle";
 import { Link } from "react-router-dom";
 
 const SiteIntro = () => {
     return (
-        <>
+        <IntroContainer>
             <Heading> Welcome to Activity-Finder!</Heading>
             <Paragraph>New in town? Don't know anybody? Love to sport? We are here for you!<br /> <Link to="/register" className="paragraph-link">Create a free account </Link> and join our community! </Paragraph>
-        </>
+        </IntroContainer>
     );
 }
 
