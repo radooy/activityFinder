3404940ddb053465e4ec7740260529cf9ca9255f
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-23 15:17:12 +0200

    Started building main layout for guests

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,21 +1,16 @@
-import { CardWrapper } from "./mainStyle";
 import SiteIntro from "../SiteIntro/SiteIntro";
+import { MainContainer, ContentWrapper } from "./mainStyle";
 
 const Main = () => {
 
-  return (
-    <>
-    <SiteIntro/>
-    <CardWrapper>
-      <p>SomeText</p>
-      <p>SomeText</p>
-      <p>SomeText</p>
-      <p>SomeText</p>
-      <p>SomeText</p>
-      <p>SomeText</p>
-    </CardWrapper>
-    </>
-  );
+    return (
+        <MainContainer>
+            <SiteIntro />
+            <ContentWrapper>
+                <img src="https://cdn.pixabay.com/photo/2014/05/21/15/28/soccer-349821_1280.jpg" alt="Activity" className="guest-page-image"></img>
+            </ContentWrapper>
+        </MainContainer>
+    );
 }
 
 export default Main;
--- client/src/components/Main/mainStyle.js
+++ client/src/components/Main/mainStyle.js
@@ -1,12 +1,25 @@
 import styled from "styled-components";
 
-const CardWrapper = styled.div`
+
+const MainContainer = styled.div`
+    margin: 20px 0;
+`
+
+const ContentWrapper = styled.div`
     width:100%;
     display:grid;
-    grid-template-columns: 33% 33% 33%;
+    grid-template-columns: 55% 45%;
     margin:10px 0;
+
+
+    .guest-page-image{
+        max-width:100%;
+        border-radius:10px;
+        opacity:70%;
+    }
 `;
 
 export {
-    CardWrapper
+    MainContainer,
+    ContentWrapper
 }
