7db802656d72fd3e937f2a0b368bfff35f839d40
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-23 16:14:04 +0200

    Added pseudo form

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -7,7 +7,15 @@ const Main = () => {
         <MainContainer>
             <SiteIntro />
             <ContentWrapper>
-                <img src="https://cdn.pixabay.com/photo/2014/05/21/15/28/soccer-349821_1280.jpg" alt="Activity" className="guest-page-image"></img>
+                <img src="https://cdn.pixabay.com/photo/2014/05/21/15/28/soccer-349821_1280.jpg" alt="Activity" className="guest-page-image"/>
+                <form>
+                    <label for="username">Username:</label>
+                    <input type="text" name="username"/><br/>
+                    <label for="username">Password:</label>
+                    <input type="password"/><br/>
+                    <input type="submit" value="Login"/>
+
+                </form>
             </ContentWrapper>
         </MainContainer>
     );
--- client/src/components/Main/mainStyle.js
+++ client/src/components/Main/mainStyle.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 const MainContainer = styled.div`
     margin: 20px 0;
 `
