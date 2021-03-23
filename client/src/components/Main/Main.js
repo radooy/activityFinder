da41148ac286fdd04d53469f16200460593a554d
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-23 16:47:10 +0200

    Implemented sample routing to see if switch works correctly

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,4 +1,7 @@
+import {Switch, Route} from "react-router-dom";
 import SiteIntro from "../SiteIntro/SiteIntro";
+import Login from "../Forms/Login";
+import Register from "../Forms/Register";
 import { MainContainer, ContentWrapper } from "./mainStyle";
 
 const Main = () => {
@@ -8,14 +11,10 @@ const Main = () => {
             <SiteIntro />
             <ContentWrapper>
                 <img src="https://cdn.pixabay.com/photo/2014/05/21/15/28/soccer-349821_1280.jpg" alt="Activity" className="guest-page-image"/>
-                <form>
-                    <label for="username">Username:</label>
-                    <input type="text" name="username"/><br/>
-                    <label for="username">Password:</label>
-                    <input type="password"/><br/>
-                    <input type="submit" value="Login"/>
-
-                </form>
+                <Switch>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/login" component={Login} />
+                </Switch>
             </ContentWrapper>
         </MainContainer>
     );
