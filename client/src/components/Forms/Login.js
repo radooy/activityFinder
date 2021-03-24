1f9a332851175b072db71f59128a794d9b7f2846
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-24 17:53:28 +0200

    Reworked login
    Still have to work on the submit handler

--- client/src/components/Forms/Login.js
+++ client/src/components/Forms/Login.js
@@ -1,17 +1,39 @@
 import { Component } from "react";
+import { Wrapper, Heading, Form, Input, Submit, Label } from "./formStyle"
 
 class Login extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            username: " ",
-            password: " "
+            username: "",
+            password: ""
         }
     }
+
+    onChangeHandler(e){
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmitHandler(e){
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <h1>Login</h1>
+            <Wrapper>
+                <Heading>Already got an account? Login from here:</Heading>
+                <Form onSubmit={this.onSubmitHandler.bind(this)}>
+                    <Label htmlFor="username">Username:</Label>
+                    <Input type="text" name="username" id="username" onChange={this.onChangeHandler.bind(this)}/>
+                    <Label htmlFor="password">Password:</Label>
+                    <Input type="password" name="password" id="password" onChange={this.onChangeHandler.bind(this)}/>
+                    <Submit value="Log in"/>
+                </Form>
+            </Wrapper>
         );
     }
 }
