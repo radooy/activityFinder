cde368d2ce6b8816b19a8b9a1b8d3d92d31b9123
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-24 18:54:58 +0200

    Reworked Register
    Still need to work on submitHandler

--- client/src/components/Forms/Register.js
+++ client/src/components/Forms/Register.js
@@ -1,17 +1,60 @@
 import { Component } from "react";
+import { Wrapper, Heading, Form, Input, Submit, Label, Select } from "./formStyle"
 
 class Register extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            username: " ",
-            password: " "
+            username: "",
+            password: "",
+            rePassword: "",
+            city: "",
+            cities: []
         }
     }
+
+    onChangeHandler(e) {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmitHandler(e) {
+        e.preventDefault();
+    }
+
+
+    componentDidMount() {
+        fetch("http://localhost:5000/api/utils/cities")
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ cities: data.cities })
+                console.log(data.cities);
+            })
+            .catch(err => console.log(err));
+    }
+
+
     render() {
         return (
-            <h1>Register</h1>
+            <Wrapper>
+                <Heading>Register:</Heading>
+                <Form onSubmit={this.onSubmitHandler.bind(this)}>
+                    <Label htmlFor="username">Username:</Label>
+                    <Input type="text" name="username" id="username" onChange={this.onChangeHandler.bind(this)} />
+                    <Label htmlFor="password">Password:</Label>
+                    <Input type="password" name="password" id="password" onChange={this.onChangeHandler.bind(this)} />
+                    <Label htmlFor="rePassword">Repeat password:</Label>
+                    <Input type="password" name="rePassword" id="rePassword" onChange={this.onChangeHandler.bind(this)} />
+                    <Label htmlFor="city">Choose your city:</Label>
+                    <Select name="city" id="city" onChange={this.onChangeHandler.bind(this)}>
+                        {this.state.cities.map((city) => <option key={city}> {city} </option>)}
+                    </Select>
+                    <Submit value="Register" />
+                </Form>
+            </Wrapper>
         );
     }
 }
