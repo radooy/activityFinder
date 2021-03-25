29cd5a2f391c1dd7252e5e2baaf7730f7f59ce2d
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-25 18:39:27 +0200

    Add guest

---
+++ client/src/components/Main/Guest/guestStyle.js
@@ -0,0 +1,31 @@
+import styled, {keyframes} from "styled-components"
+
+const slideFromLeft = keyframes`
+    0%{
+        transform: translateX(-100%);
+    }
+
+    100%{
+        transform: translateX(0);
+    }
+`
+
+const ContentWrapper = styled.div`
+width:100%;
+display:grid;
+grid-template-columns: 55% 45%;
+margin:10px 0;
+
+
+.guest-page-image{
+    max-width:100%;
+    border-radius:10px;
+    opacity:85%;
+    animation-name: ${slideFromLeft};
+    animation-duration:2s;
+}
+`;
+
+export {
+    ContentWrapper
+}
