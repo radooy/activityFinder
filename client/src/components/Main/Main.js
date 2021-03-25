c0b095d6fe29b9efa4a128c72605e5c019fd6fa9
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-25 18:39:47 +0200

    Update main component

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,9 +1,5 @@
-import {Switch, Route} from "react-router-dom";
-import SiteIntroHead from "../SiteIntro/SiteIntroHead";
-import SiteIntroBody from "../SiteIntro/SiteIntroBody";
-import Login from "../Forms/Login";
-import Register from "../Forms/Register";
-import { MainContainer, ContentWrapper } from "./mainStyle";
+import { MainContainer } from "./mainStyle";
+import Guest from "./Guest/Guest";
 
 const Main = () => {
     
@@ -11,15 +7,7 @@ const Main = () => {
         <MainContainer>
 
         {/* If not logged in */}
-            <SiteIntroHead />
-            <ContentWrapper>
-            <img src="https://cdn.pixabay.com/photo/2014/05/21/15/28/soccer-349821_1280.jpg" alt="Activity" className="guest-page-image"/>
-                <Switch>
-                    <Route exact path="/" component={SiteIntroBody} />
-                    <Route path="/register" component={Register}/>
-                    <Route path="/login" component={Login} />
-                </Switch>
-            </ContentWrapper>
+            <Guest/>
         {/* /If not logged in */}
 
 
--- client/src/components/Main/mainStyle.js
+++ client/src/components/Main/mainStyle.js
@@ -1,36 +1,13 @@
 import styled , {keyframes} from "styled-components";
 
-const slideFromLeft = keyframes`
-    0%{
-        transform: translateX(-100%);
-    }
 
-    100%{
-        transform: translateX(0);
-    }
-`
 
 const MainContainer = styled.div`
     margin: 20px 0;
 `
 
-const ContentWrapper = styled.div`
-    width:100%;
-    display:grid;
-    grid-template-columns: 55% 45%;
-    margin:10px 0;
 
-
-    .guest-page-image{
-        max-width:100%;
-        border-radius:10px;
-        opacity:85%;
-        animation-name: ${slideFromLeft};
-        animation-duration:2s;
-    }
-`;
 
 export {
-    MainContainer,
-    ContentWrapper
+    MainContainer
 }
