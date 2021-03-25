038728599471bdfc857c681c788d0e159d5d5307
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-25 13:04:01 +0200

    Added cookie-parser

--- server/config/express.js
+++ server/config/express.js
@@ -2,10 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("../routes");
 const { auth } = require("../middlewares/auth");
+const cookieParser = require("cookie-parser")
 
 function setUp(app) {
     app.use(express.urlencoded({extended:true}));
     app.use(express.json());
+    app.use(cookieParser());
     app.use(cors());
 
     app.use(auth);
