bcedff54d75cf02d3dfb3c451104832940bad6b8
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-25 13:02:47 +0200

    Update Footer.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom"
 const Footer = () => {
     return(
     <StyledFooter>
-        <span><Link to="/contact-us" className="footer-link">Contact Us</Link></span>
         <span>Copyright <Link to ={{pathname:"https://github.com/radooy"}} target="_blank" className="footer-link">Radooy</Link> 2021 &copy;</span>
         <span><Link to="/about" className="footer-link">About</Link></span>
     </StyledFooter>
