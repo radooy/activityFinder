adbdaae866c95aefbe718e9bfa4b2621f4be8d38
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-28 19:29:10 +0300

    Create homeStyle.js

---
+++ client/src/components/Main/User/Home/homeStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+    margin-top:30px;
+`;
+
+const Heading = styled.h1`
+    text-align: center
+`;
+
+export {
+    Wrapper,
+    Heading
+}
