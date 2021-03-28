a199a2186918fba327d28ea288d1c1286d7c3011
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-28 19:29:07 +0300

    Update Home.js

--- client/src/components/Main/User/Home/Home.js
+++ client/src/components/Main/User/Home/Home.js
@@ -1,8 +1,17 @@
-
+import ActivitySection from "./AcctivitiesSection/ActivitiesSection"
+import {Wrapper, Heading} from "./homeStyle"
 
 const HomePage = () => {
+
+    let cityName = localStorage.getItem("city");
+
     return(
-        <div>HOME</div>
+
+        <Wrapper>
+            <Heading>Activities in {cityName}</Heading>
+            <ActivitySection/>
+        </Wrapper>
+
     );
 }
 
