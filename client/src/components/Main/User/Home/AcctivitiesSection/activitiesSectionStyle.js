096d2a91383653a776c435e5eb86fe608a14daf1
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-28 19:28:24 +0300

    Create activitiesSectionStyle.js

---
+++ client/src/components/Main/User/Home/AcctivitiesSection/activitiesSectionStyle.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const ActivitiesWrappper = styled.section`
+    max-width: 95%;
+    width: 100%;
+    display:grid;
+    grid-template-columns: repeat(4, 1fr);
+    margin:auto;
+`
+
+export {ActivitiesWrappper}
