f81e73968fe28ae9ccc76221c15daeebd70e1b6f
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-28 20:13:41 +0300

    Updated user home page

--- client/src/components/Main/User/Home/AcctivitiesSection/ActivitiesSection.js
+++ client/src/components/Main/User/Home/AcctivitiesSection/ActivitiesSection.js
@@ -1,53 +1,29 @@
+import { useEffect, useState } from "react"
 import Activity from "../Activity/Activity"
 import {ActivitiesWrappper} from "./activitiesSectionStyle"
 
 const ActivitiesSection = () => {
+    const [state, setState] = useState([]);
+    
+    useEffect(()=>{
+        fetch("http://localhost:5000/api/publications")
+            .then(res=> res.json())
+            .then(data=>{
+                setState(data.publications)
+            })
+    },[])
+
+
     return(
         <ActivitiesWrappper>
-            <Activity
-            sport="Football"
-            nameOfUser="Rado"
-            description="Some description will be added here! HereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHere"
-            date="21.12.2021 13:12"
-            city="Sofia"
-            peopleNeeded="3"
-            peopleApplied="0"
-            phoneNumber= "0898999999"
-            imgUrl="https://ec.europa.eu/eurostat/documents/6921402/9104237/Shutterstock_Lisa_Kolbasa.png/f988f8b6-4138-4a91-9761-885bacab0ce2?t=1533725002000"
-            />
-            <Activity
-            sport="Football"
-            nameOfUser="Rado"
-            description="Some description will be added here!"
-            date="21.12.2021 13:12"
-            city="Sofia"
-            peopleNeeded="3"
-            peopleApplied="0"
-            phoneNumber= "0898999999"
-            imgUrl="https://ec.europa.eu/eurostat/documents/6921402/9104237/Shutterstock_Lisa_Kolbasa.png/f988f8b6-4138-4a91-9761-885bacab0ce2?t=1533725002000"
-            />
-            <Activity
-            sport="Football"
-            nameOfUser="Rado"
-            description="Some description will be added here!"
-            date="21.12.2021 13:12"
-            city="Sofia"
-            peopleNeeded="3"
-            peopleApplied="0"
-            phoneNumber= "0898999999"
-            imgUrl="https://ec.europa.eu/eurostat/documents/6921402/9104237/Shutterstock_Lisa_Kolbasa.png/f988f8b6-4138-4a91-9761-885bacab0ce2?t=1533725002000"
-            />
-             <Activity
-            sport="Football"
-            nameOfUser="Rado"
-            description="Some description will be added here!"
-            date="21.12.2021 13:12"
-            city="Sofia"
-            peopleNeeded="3"
-            peopleApplied="0"
-            phoneNumber= "0898999999"
-            imgUrl="https://ec.europa.eu/eurostat/documents/6921402/9104237/Shutterstock_Lisa_Kolbasa.png/f988f8b6-4138-4a91-9761-885bacab0ce2?t=1533725002000"
-            />
+            {state.map(activity => <Activity
+                key={activity._id}
+                sport={activity.sport}
+                description={activity.description}
+                city={activity.city}
+                date={activity.date}
+                peopleApplied={activity.peopleApplied.length}
+                peopleNeeded={activity.peopleNeeded}/>)}
         </ActivitiesWrappper>
     )
 }
--- client/src/components/Main/User/Home/AcctivitiesSection/activitiesSectionStyle.js
+++ client/src/components/Main/User/Home/AcctivitiesSection/activitiesSectionStyle.js
@@ -1,4 +1,14 @@
-import styled from "styled-components"
+import styled, {keyframes} from "styled-components"
+
+const fadeIn = keyframes`
+    0%{
+         opacity: 0;
+    }
+
+   100%{
+        opacity: 1;
+    }
+`
 
 const ActivitiesWrappper = styled.section`
     max-width: 95%;
@@ -6,6 +16,8 @@ const ActivitiesWrappper = styled.section`
     display:grid;
     grid-template-columns: repeat(4, 1fr);
     margin:auto;
+    animation-name: ${fadeIn};
+    animation-duration: 5s;
 `
 
 export {ActivitiesWrappper}
