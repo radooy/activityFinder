d5718712573002721941453f707cb8e25fadc46f
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-28 19:28:18 +0300

    Added demo activities

---
+++ client/src/components/Main/User/Home/AcctivitiesSection/ActivitiesSection.js
@@ -0,0 +1,55 @@
+import Activity from "../Activity/Activity"
+import {ActivitiesWrappper} from "./activitiesSectionStyle"
+
+const ActivitiesSection = () => {
+    return(
+        <ActivitiesWrappper>
+            <Activity
+            sport="Football"
+            nameOfUser="Rado"
+            description="Some description will be added here! HereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHereHere"
+            date="21.12.2021 13:12"
+            city="Sofia"
+            peopleNeeded="3"
+            peopleApplied="0"
+            phoneNumber= "0898999999"
+            imgUrl="https://ec.europa.eu/eurostat/documents/6921402/9104237/Shutterstock_Lisa_Kolbasa.png/f988f8b6-4138-4a91-9761-885bacab0ce2?t=1533725002000"
+            />
+            <Activity
+            sport="Football"
+            nameOfUser="Rado"
+            description="Some description will be added here!"
+            date="21.12.2021 13:12"
+            city="Sofia"
+            peopleNeeded="3"
+            peopleApplied="0"
+            phoneNumber= "0898999999"
+            imgUrl="https://ec.europa.eu/eurostat/documents/6921402/9104237/Shutterstock_Lisa_Kolbasa.png/f988f8b6-4138-4a91-9761-885bacab0ce2?t=1533725002000"
+            />
+            <Activity
+            sport="Football"
+            nameOfUser="Rado"
+            description="Some description will be added here!"
+            date="21.12.2021 13:12"
+            city="Sofia"
+            peopleNeeded="3"
+            peopleApplied="0"
+            phoneNumber= "0898999999"
+            imgUrl="https://ec.europa.eu/eurostat/documents/6921402/9104237/Shutterstock_Lisa_Kolbasa.png/f988f8b6-4138-4a91-9761-885bacab0ce2?t=1533725002000"
+            />
+             <Activity
+            sport="Football"
+            nameOfUser="Rado"
+            description="Some description will be added here!"
+            date="21.12.2021 13:12"
+            city="Sofia"
+            peopleNeeded="3"
+            peopleApplied="0"
+            phoneNumber= "0898999999"
+            imgUrl="https://ec.europa.eu/eurostat/documents/6921402/9104237/Shutterstock_Lisa_Kolbasa.png/f988f8b6-4138-4a91-9761-885bacab0ce2?t=1533725002000"
+            />
+        </ActivitiesWrappper>
+    )
+}
+
+export default ActivitiesSection
