079f89850495c1ef9e23a4f8576ba2559b74630a
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-29 20:39:59 +0300

    Update Header.js

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,23 +6,23 @@ import { StyledHeader, StyledUl, StyledLi, Logo } from "./headerStyle";
 
 const Header = () => {
     const userInfo = useContext(UserContext);
-    const logOut= ()=>{
+    const logOut = () => {
         userInfo.logOut();
     }
-    
+
     return (
         <StyledHeader>
             <Link to="/" className="logo"><Logo src="https://i.postimg.cc/65Jr9g81/dark-logo-transparent.png" alt="Activity-Finder" /></Link>
             <StyledUl>
                 {userInfo.loggedIn && <>
-                <Link to="/create" className="header-link"> <StyledLi>Create a publication</StyledLi> </Link>
-                <Link to="/my-profile" className="header-link"> <StyledLi>My profile</StyledLi> </Link>
-                <StyledLi className="header-link logout" onClick={()=>logOut()} >Logout</StyledLi>
+                    <Link to="/create" className="header-link"> <StyledLi>Create a publication</StyledLi> </Link>
+                    <Link to="/my-profile" className="header-link"> <StyledLi>My profile</StyledLi> </Link>
+                    <StyledLi className="header-link logout" onClick={() => logOut()} >Logout</StyledLi>
                 </>
                 }
-                {userInfo.loggedIn ===false && <>
-                <Link to="/register" className="header-link"> <StyledLi>Register</StyledLi> </Link>
-                <Link to="/login" className="header-link"> <StyledLi>Login</StyledLi> </Link>
+                {userInfo.loggedIn === false && <>
+                    <Link to="/register" className="header-link"> <StyledLi>Register</StyledLi> </Link>
+                    <Link to="/login" className="header-link"> <StyledLi>Login</StyledLi> </Link>
                 </>}
             </StyledUl>
         </StyledHeader>
