5565480edb948eb8db1589ab60533f98feb6c0b2
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-29 20:45:34 +0300

    Update Activity.js

--- client/src/components/Main/User/Home/Activity/Activity.js
+++ client/src/components/Main/User/Home/Activity/Activity.js
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useState } from "react"
 import {ActivityWrapper, Heading, InfoFor} from "./activityStyle"
 
 const Activity = ({nameOfUser, sport, date, description, peopleNeeded, city, phoneNumber, imgUrl, peopleApplied}) => {
     let [detailed, setDetailed] = useState(false)
     
-    let onClickHandler = () => {
+    const onClickHandler = (e) => {
         setDetailed(!detailed);
     }
+
     return(
-        <ActivityWrapper onClick = {()=>onClickHandler()}>
+        <ActivityWrapper onClick = {onClickHandler}>
             <Heading>{sport}</Heading>
             {detailed && <img className="activity-img" src={imgUrl} alt="Activity"/>}   
             {detailed && <InfoFor><b>Name of creator:</b> {nameOfUser}</InfoFor>}
