ba9703de17a3b49506f1c4a0f3f91bbacd853023
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-29 12:50:24 +0300

    Update ActivitiesSection.js

--- client/src/components/Main/User/Home/AcctivitiesSection/ActivitiesSection.js
+++ client/src/components/Main/User/Home/AcctivitiesSection/ActivitiesSection.js
@@ -4,19 +4,24 @@ import {ActivitiesWrappper} from "./activitiesSectionStyle"
 
 const ActivitiesSection = () => {
     const [state, setState] = useState([]);
-    
+
+    console.log(state);
     useEffect(()=>{
-        fetch("http://localhost:5000/api/publications")
+        fetch("http://localhost:5000/api/publications",{
+            credentials:"include"
+        })
             .then(res=> res.json())
             .then(data=>{
+                if (data.message) throw data.message;
                 setState(data.publications)
             })
+            .catch(err=>console.log(err))
     },[])
 
 
     return(
         <ActivitiesWrappper>
-            {state.map(activity => <Activity
+            {state && state.map(activity => <Activity
                 key={activity._id}
                 sport={activity.sport}
                 description={activity.description}
