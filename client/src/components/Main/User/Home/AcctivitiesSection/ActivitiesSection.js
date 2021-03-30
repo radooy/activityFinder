5520a796ed748d490e63553d5144a8cacab1b859
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-30 12:20:43 +0300

    Update ActivitiesSection.js
    No longer invokes the error message for key need

--- client/src/components/Main/User/Home/AcctivitiesSection/ActivitiesSection.js
+++ client/src/components/Main/User/Home/AcctivitiesSection/ActivitiesSection.js
@@ -2,46 +2,47 @@ import { useEffect, useState, useContext } from "react"
 import { Link } from "react-router-dom"
 import UserContext from "../../../../Context/Context";
 import Activity from "../Activity/Activity"
-import {ActivitiesWrappper, NoActivitiesDiv} from "./activitiesSectionStyle"
+import { ActivitiesWrappper, NoActivitiesDiv } from "./activitiesSectionStyle"
 
 const ActivitiesSection = () => {
 
     const [state, setState] = useState([]);
     const userInfo = useContext(UserContext);
 
-    useEffect(()=>{
-        fetch("http://localhost:5000/api/publications",{
-            credentials:"include"
+    useEffect(() => {
+        fetch("http://localhost:5000/api/publications", {
+            credentials: "include"
         })
-            .then(res=> res.json())
-            .then(data=>{
+            .then(res => res.json())
+            .then(data => {
                 if (data.message) throw data.message;
                 setState(data.publications)
             })
-            .catch(err=>{
+            .catch(err => {
                 userInfo.logOut();
                 console.log(err)
             })
-    },[userInfo])
+    }, [userInfo])
 
 
-    return(
-        <ActivitiesWrappper display = {state.length===0 ? "block" : "grid"}>
-            {state.length>0 ? state.map(activity =>
-            <>
-            <Link to={`/details/${activity._id}`} key={activity.id} className="details-link">
-            <Activity
-                key={activity._id}
-                sport={activity.sport}
-                description={activity.description}
-                city={activity.city}
-                date={activity.date}
-                peopleApplied={activity.peopleApplied.length}
-                peopleNeeded={activity.peopleNeeded}
-            /> 
-            </Link>
-            
-            </>):<NoActivitiesDiv>There are currently no activities to show!</NoActivitiesDiv>}
+    return (
+        <ActivitiesWrappper display={state.length === 0 ? "block" : "grid"}>
+            {state.length > 0 ? state.map(activity =>
+
+                <div key={activity._id}>
+
+                    <Link to={`/details/${activity._id}`} className="details-link">
+                        <Activity
+                            sport={activity.sport}
+                            description={activity.description}
+                            city={activity.city}
+                            date={activity.date}
+                            peopleApplied={activity.peopleApplied.length}
+                            peopleNeeded={activity.peopleNeeded}
+                        />
+                    </Link>
+
+                </div>) : <NoActivitiesDiv>There are currently no activities to show!</NoActivitiesDiv>}
         </ActivitiesWrappper>
     )
 }
