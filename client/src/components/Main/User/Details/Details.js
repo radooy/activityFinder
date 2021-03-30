af54de45a13d317418472c57e74a44523e4fa263
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-30 15:33:55 +0300

    Update Details.js

--- client/src/components/Main/User/Details/Details.js
+++ client/src/components/Main/User/Details/Details.js
@@ -1,13 +1,52 @@
-
+import { useState, useEffect } from "react"
+import { Redirect } from "react-router-dom"
+import Activity from "../Home/Activity/Activity"
+import { DetailsWrapper } from "./detailsStyle"
 
 const Details = (props) => {
     const id = props.match.params.id;
 
+    const [activity, setActivity] = useState({});
+    const [redirect, setRedirect] = useState("");
+    const [peopleApplied, setPeopleApplied] = useState(0)
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/api/publications/${id}`, {
+            credentials: "include"
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.message) throw data.message;
+                setActivity(data.publication);
+                setPeopleApplied(data.publication.peopleApplied.length);
+            })
+            .catch(err => {
+                console.log(err)
+                setRedirect("/error");
+            })
+    }, [id])
+
+    if (redirect.length > 0) {
+        return <Redirect to={redirect} />
+    }
+
     return (
-        <div>
-            Details page for {id}
-        </div>
+        <DetailsWrapper>
+            <Activity
+                detailed={true}
+                nameOfUser={activity.nameOfUser}
+                sport={activity.sport}
+                description={activity.description}
+                city={activity.city}
+                date={activity.date}
+                peopleApplied={peopleApplied}
+                peopleNeeded={activity.peopleNeeded}
+                imgUrl={activity.imgUrl}
+                phoneNumber={activity.phoneNumber}
+            />
+        </DetailsWrapper>
+
     );
 }
 
-export default Details 
+export default Details
