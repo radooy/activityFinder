9dafe080e4cf84e8823db5c8246e11da22ff1385
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-03-31 19:17:39 +0300

    Create Edit page for creator only

---
+++ client/src/components/Main/User/Edit/editStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const Heading = styled.h1`
+    text-align:center;
+    font-size:40px;
+    font-family: 'Audiowide', cursive;
+    margin-bottom:100px;
+`
+
+export {
+    Heading,
+}
