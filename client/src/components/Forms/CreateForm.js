7ea9aceaa935f846e20e1bdb6d54ba007c90b2f1
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-04-01 11:41:31 +0300

    Reworked create form and edit
    Instead of creating a new create form in the edit component now edit is just using the already created createForm

--- client/src/components/Forms/CreateForm.js
+++ client/src/components/Forms/CreateForm.js
@@ -1,5 +1,5 @@
 import { Component } from "react"
-import {Redirect} from "react-router-dom"
+import { Redirect } from "react-router-dom"
 import { Wrapper, Form, Input, Submit, Label, Select } from "./formStyle"
 
 class CreateForm extends Component {
@@ -11,7 +11,7 @@ class CreateForm extends Component {
             sport: "Football",
             date: null,
             description: "",
-            peopleNeeded: 0,
+            peopleNeeded: 1,
             phoneNumber: "",
             imgUrl: "",
             city: "Sofia",
@@ -37,7 +37,7 @@ class CreateForm extends Component {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ 
+            body: JSON.stringify({
                 nameOfUser: this.state.nameOfUser,
                 sport: this.state.sport,
                 date: this.state.date,
@@ -63,6 +63,39 @@ class CreateForm extends Component {
             });
     }
 
+    onEditHandler(e){
+        e.preventDefault();
+        
+        fetch(`http://localhost:5000/api/publications/${this.props.id}`, {
+            credentials: "include",
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                nameOfUser: this.state.nameOfUser,
+                sport: this.state.sport,
+                date: this.state.date,
+                description: this.state.description,
+                peopleNeeded: Number(this.state.peopleNeeded),
+                phoneNumber: this.state.phoneNumber,
+                city: this.state.city,
+                imgUrl: this.state.imgUrl
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.message) throw data.message;
+                console.log('Success:', data);
+                this.setState({
+                    redirect:`/details/${this.props.id}`
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
 
     componentDidMount() {
         fetch("http://localhost:5000/api/utils/cities")
@@ -78,27 +111,43 @@ class CreateForm extends Component {
                 this.setState({ sports: data.sports })
             })
             .catch(err => console.log(err));
+
+        if(this.props.id){
+            fetch(`http://localhost:5000/api/publications/${this.props.id}`)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data.publication.peopleNeeded)
+                    this.setState({
+                        nameOfUser:data.publication.nameOfUser,
+                        date: data.publication.date,
+                        description: data.publication.description,
+                        peopleNeeded: Number(data.publication.peopleNeeded),
+                        phoneNumber: data.publication.phoneNumber,
+                        imgUrl: data.publication.imgUrl
+                    })
+                });
+        }
     }
 
 
     render() {
         if (this.state.redirect) {
-            return <Redirect to= {this.state.redirect}/>
+            return <Redirect to={this.state.redirect} />
         }
         return (
             <Wrapper>
-                <Form onSubmit={this.onSubmitHandler.bind(this)}>
+                <Form onSubmit={this.props.id ? this.onEditHandler.bind(this) : this.onSubmitHandler.bind(this)}>
                     <Label htmlFor="nameOfUser">Name of creator:</Label>
-                    <Input type="text" name="nameOfUser" id="nameOfUser" onChange={this.onChangeHandler.bind(this)} />
+                    <Input type="text" name="nameOfUser" id="nameOfUser" defaultValue={this.state.nameOfUser} onChange={this.onChangeHandler.bind(this)} />
 
                     <Label htmlFor="description">Short description:</Label>
-                    <Input type="text" name="description" id="description" onChange={this.onChangeHandler.bind(this)} />
+                    <Input type="text" name="description" id="description" defaultValue={this.state.description} onChange={this.onChangeHandler.bind(this)} />
 
                     <Label htmlFor="date">Date:</Label>
                     <Input type="datetime-local" name="date" id="date" onChange={this.onChangeHandler.bind(this)} />
 
                     <Label htmlFor="peopleNeeded">People needed:</Label>
-                    <Input type="number" min="1" max="20" name="peopleNeeded" id="peopleNeeded" onChange={this.onChangeHandler.bind(this)} />
+                    <Input type="number" min="1" max="20" name="peopleNeeded" id="peopleNeeded" value={this.state.peopleNeeded} onChange={this.onChangeHandler.bind(this)} />
 
                     <Label htmlFor="sport">Choose sport:</Label>
                     <Select name="sport" id="sport" onChange={this.onChangeHandler.bind(this)}>
@@ -111,12 +160,12 @@ class CreateForm extends Component {
                     </Select>
 
                     <Label htmlFor="phoneNumber">Phone number:</Label>
-                    <Input type="text" name="phoneNumber" id="phoneNumber" onChange={this.onChangeHandler.bind(this)} />
+                    <Input type="text" name="phoneNumber" id="phoneNumber" defaultValue={this.state.phoneNumber} onChange={this.onChangeHandler.bind(this)} />
 
                     <Label htmlFor="imgUrl">Image Url:</Label>
-                    <Input type="text" name="imgUrl" id="imgUrl" onChange={this.onChangeHandler.bind(this)} />
+                    <Input type="text" name="imgUrl" id="imgUrl" defaultValue={this.state.imgUrl} onChange={this.onChangeHandler.bind(this)} />
 
-                    <Submit className="activity-submit" value="Create" />
+                    <Submit className="activity-submit" value={this.props.buttonName || "Create"} />
                 </Form>
             </Wrapper>
         )
--- client/src/components/Main/User/Edit/Edit.js
+++ client/src/components/Main/User/Edit/Edit.js
@@ -1,124 +1,13 @@
-import { useEffect, useState } from "react";
-import { Redirect } from "react-router-dom"
-import { Wrapper, Form, Label, Input, Select, Submit} from "../../../Forms/formStyle"
 import { Heading } from "./editStyle"
+import CreateForm from "../../../Forms/CreateForm"
 
 const Edit = (props)=>{
     const id = props.match.params.id;
-    const [publication, setPublication] = useState({});
-    const [cities, setCities] = useState([]);
-    const [sports, setSports] = useState([]);
-    const [redirect, setRedirect] = useState("");
-
-    const [nameOfUser, setNameOfUser] = useState("");
-    const [sport, setSport]=useState("Football")
-    const [date, setDate]=useState("")
-    const [description, setDescription]=useState("")
-    const [peopleNeeded, setPeopleNeeded]=useState(0)
-    const [phoneNumber, setPhoneNumber]=useState("")
-    const [city, setCity]=useState("Sofia")
-    const [imgUrl, setImgUrl]=useState("")
-
-    useEffect(()=>{
-        fetch(`http://localhost:5000/api/publications/${id}`)
-            .then(res=>res.json())
-            .then(data=>{
-                setPublication(data.publication);
-                setNameOfUser(data.publication.nameOfUser);
-                setDate(data.publication.date);
-                setDescription(data.publication.description);
-                setPeopleNeeded(data.publication.peopleNeeded);
-                setPhoneNumber(data.publication.phoneNumber);
-                setImgUrl(data.publication.imgUrl);
-
-            });
-
-        fetch("http://localhost:5000/api/utils/cities")
-            .then(res => res.json())
-            .then((data) => {
-                setCities(data.cities);
-            })
-            .catch(err => console.log(err));
-
-        fetch("http://localhost:5000/api/utils/sports")
-            .then(res => res.json())
-            .then((data) => {
-                setSports(data.sports);
-            })
-            .catch(err => console.log(err));
-    },[]);
-
-    const onEditHandler = (e) => {
-        e.preventDefault();
-        
-        fetch(`http://localhost:5000/api/publications/${id}`, {
-            credentials: "include",
-            method: "PATCH",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ 
-                nameOfUser,
-                sport,
-                date,
-                description,
-                peopleNeeded: Number(peopleNeeded),
-                phoneNumber,
-                city,
-                imgUrl
-            })
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.message) throw data.message;
-                console.log('Success:', data);
-                setRedirect(`/details/${id}`);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    };
-
-    if (redirect.length>0) {
-        return <Redirect to= {redirect}/>
-    }
 
     return (
         <>
         <Heading>Edit your publication</Heading>
-        <Wrapper>
-                <Form onSubmit={onEditHandler}>
-                    <Label htmlFor="nameOfUser">Name of creator:</Label>
-                    <Input type="text" name="nameOfUser" id="nameOfUser" defaultValue={publication.nameOfUser} onChange={(e)=>setNameOfUser(e.target.value)}  />
-
-                    <Label htmlFor="description">Short description:</Label>
-                    <Input type="text" name="description" id="description" defaultValue={publication.description}  onChange={(e)=>setDescription(e.target.value)}/>
-
-                    <Label htmlFor="date">Date:</Label>
-                    <Input type="datetime-local" name="date" id="date" onChange={(e)=>setDate(e.target.value)} />
-
-                    <Label htmlFor="peopleNeeded">People needed:</Label>
-                    <Input type="number" min="1" max="20" name="peopleNeeded" id="peopleNeeded" defaultValue={publication.peopleNeeded} onChange={(e)=>setPeopleNeeded(e.target.value)} />
-
-                    <Label htmlFor="sport">Choose sport:</Label>
-                    <Select name="sport" id="sport" onChange={(e)=>setSport(e.target.value)}>
-                        {sports.map((sport) => <option key={sport} value={sport}> {sport} </option>)}
-                    </Select>
-
-                    <Label htmlFor="city">Choose your city:</Label>
-                    <Select name="city" id="city" onChange={(e)=>setCity(e.target.value)}>
-                        {cities.map((city) => <option key={city} value={city}> {city} </option>)}
-                    </Select>
-
-                    <Label htmlFor="phoneNumber">Phone number:</Label>
-                    <Input type="text" name="phoneNumber" id="phoneNumber" defaultValue={publication.phoneNumber}  onChange={(e)=>setPhoneNumber(e.target.value)}/>
-
-                    <Label htmlFor="imgUrl">Image Url:</Label>
-                    <Input type="text" name="imgUrl" id="imgUrl" defaultValue={publication.imgUrl}  onChange={(e)=>setImgUrl(e.target.value)}/>
-
-                    <Submit className="activity-submit" value="Create" />
-                </Form>
-            </Wrapper>
+        <CreateForm id={id} buttonName="Edit" />
         </>
     );
 }
