25b268a00fcb572b5a9b2626a06b340a37ed08ef
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-04-07 13:54:47 +0300

    Added React-Hot-Toaster

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,17 @@ import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
 import About from "./components/About/About"
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
     <div className="site-wrapper">
       <Header/>
-
       <Switch>
         <Route path="/about" component={About}/>
         <Route path="/" component={Main}/>
       </Switch>
-
+      <Toaster/>
       <Footer/>
     </div>
   );
