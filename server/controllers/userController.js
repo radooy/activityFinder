9cb774812e17401e1fcc857a1d7b1e866463fe8e
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-04-07 16:59:28 +0300

    Update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const userService = require("../services/userService");
+const { isAuth } = require("../middlewares/auth")
 
-router.get("/:userid", (req,res)=>{
+router.get("/:userid", isAuth, (req,res)=>{
     let id = req.params.userid;
     userService.getOne(id)
         .then(user=>{
@@ -13,7 +14,7 @@ router.get("/:userid", (req,res)=>{
         })
 })
 
-router.get("/:userId/publicationsMade", (req,res)=>{
+router.get("/:userId/publicationsMade", isAuth, (req,res)=>{
     let userId = req.params.userId;
     userService.getUserPublications(userId)
         .then(pubs=>{
@@ -25,7 +26,7 @@ router.get("/:userId/publicationsMade", (req,res)=>{
         })
 });
 
-router.get("/:userId/publicationsApplied", (req,res)=>{
+router.get("/:userId/publicationsApplied", isAuth, (req,res)=>{
     let userId = req.params.userId;
     userService.getAppliedForPublications(userId)
         .then(pubs=>{
@@ -37,7 +38,7 @@ router.get("/:userId/publicationsApplied", (req,res)=>{
         })
 })
 
-router.delete("/:userId/delete", (req,res)=>{
+router.delete("/:userId/delete", isAuth, (req,res)=>{
     let userId = req.params.userId;
     userService.remove(userId)  
         .then(()=>{
--- server/utils/errorMessages.js
+++ server/utils/errorMessages.js
@@ -1,7 +1,7 @@
 module.exports = {
     LOGIN_FAIL : "Username or password is incorrect!",
     REGISTER_USERNAME_ALREADY_IN_USE : "Username is already taken",
-    MONGOOSE_USERNAME_VALIDATION : "Username should start with lowercase letter and contain only latin letters, numbers or \"-\", \"_\", \".\" symbols, and should be between 4 and 20 characters long!",
+    MONGOOSE_USERNAME_VALIDATION : "Username should start with lowercase letter and contain only latin letters, numbers or '.', '-', '_' symbols, and should be between 4 and 20 characters long!",
     MONGOOSE_PASSWORD_VALIDATION : "Password should contain only latin letters and numbers and should be atleast 6 characters long!",
     MONGOOSE_NAME_VALIDATION : "Name of user should contain only latin letters and a single space between first and last name!",
     MONGOOSE_DESC_VALIDATION: "Description should contain latin letters and should be between 8 and 200 characters long!",
