8e7343d5b887d6c02174a6ba3042e3eee11cbabf
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-04-09 17:01:11 +0300

    Update
    Some of the plain text is now gotten from the utils

--- client/src/components/Main/Guest/SiteIntro/SiteIntroBody.js
+++ client/src/components/Main/Guest/SiteIntro/SiteIntroBody.js
@@ -1,11 +1,12 @@
-import {Article} from "./siteIntroStyle";
+import { Article } from "./siteIntroStyle";
+import { siteIntroBody } from "../../../../utils/plainText";
 
 const SiteIntroBody = ()=>{
     return(
         <Article>
-            Members of our page get unlimited access to search for publications from all of the district towns in Bulgaria from all users or even create their own publications to find a mate to sport with!
+            {siteIntroBody}
         </Article>
     );
-}
+};
 
 export default SiteIntroBody
--- client/src/components/Main/Guest/SiteIntro/SiteIntroHead.js
+++ client/src/components/Main/Guest/SiteIntro/SiteIntroHead.js
@@ -1,13 +1,23 @@
 import { IntroContainer, Heading, Paragraph } from "./siteIntroStyle";
 import { Link } from "react-router-dom";
+import { welcomeGuestHeading } from "../../../../utils/plainText";
 
 const SiteIntro = () => {
     return (
         <IntroContainer>
-            <Heading> Welcome to Activity-Finder!</Heading>
-            <Paragraph>New in town? Don't know anybody? Love to sport? We are here for you!<br /><Link to="/register" className="paragraph-link">Create a free account</Link> and join our community!<br/> <br/> Already got an account?<br/> Click <Link to="/login" className="paragraph-link">here</Link> to sign in and discover all the published activities!</Paragraph>
+            <Heading>{welcomeGuestHeading}</Heading>
+            <Paragraph>
+                New in town? Don't know anybody? Love to sport? We are here for you!<br />
+                    <Link to="/register" className="paragraph-link">
+                    Create a free account</Link> and join our community!<br/>
+                <br/>
+                Already got an account?<br/>
+                Click <Link to="/login" className="paragraph-link">
+                        here 
+                        </Link> to sign in and discover all the published activities!
+            </Paragraph>
         </IntroContainer>
     );
-}
+};
 
 export default SiteIntro
