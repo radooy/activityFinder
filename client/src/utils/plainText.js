7723c4aeac618e25ba804997163be9b2ae8f4cc0
Radostin Kirilov <65726712+radooy@users.noreply.github.com>
2021-04-09 16:55:50 +0300

    Create plainText.js
    Moved the siteIntroBody plain text and the guest heading

---
+++ client/src/utils/plainText.js
@@ -0,0 +1,8 @@
+const siteIntroBody = "Members of our page get unlimited access to search for publications from all of the district towns in Bulgaria from all users or even create their own publications to find a mate to sport with!";
+const welcomeGuestHeading = " Welcome to Activity-Finder!";
+
+
+export {
+    siteIntroBody,
+    welcomeGuestHeading
+}
