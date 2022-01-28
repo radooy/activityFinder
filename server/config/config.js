c54e9a361370797a61e795f28873ea3ad4e2c68f
Radostin Kirilov <radostin.kirilov@ad.mentormate.bg>
2022-01-28 12:42:37 +0200

    Updated the connection string

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 5000,
     LOCAL_DB_URI: "mongodb://localhost:27017/activityFinder",
-    ATLAS_DB_URI: "mongodb+srv://testuser:test123123@my-cluster.d6gp1.mongodb.net/activityFinder?retryWrites=true&w=majority",
+    ATLAS_DB_URI: "mongodb+srv://test:test1234@cluster0.izxkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     SALT_ROUNDS: 9,
     SECRET: "VeryBigSecret12321ImTellingYou9876543TryToFindMeOut8282215"
 }
