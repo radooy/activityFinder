d7867724c0dd2a2f43ca0d38cb6cf9ae654efc5b
Radostin Kirilov <radostin.kirilov@ad.mentormate.bg>
2022-02-18 13:44:54 +0200

    Year update

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import StyledFooter from "./footerStyle";
 import { Link } from "react-router-dom";
 const Footer = () => {
+    const year = new Date().getFullYear();
+    
     return(
     <StyledFooter>
-        <span>Copyright <Link to ={{pathname:"https://github.com/radooy"}} target="_blank" className="footer-link">Radooy</Link> 2021 &copy;</span>
+        <span>Copyright <Link to ={{pathname:"https://github.com/radooy"}} target="_blank" className="footer-link">Radooy</Link> {year} &copy;</span>
         <span><Link to="/about" className="footer-link">About</Link></span>
     </StyledFooter>
     );
