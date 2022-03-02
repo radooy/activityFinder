30c52de6555707c0e2d30d9ce5c6152de56813f5
Radostin Kirilov <radostin.kirilov@ad.mentormate.bg>
2022-03-02 17:31:29 +0200

    Replaced the dataContext with the dataSlice state values of sports and citites

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
+import { useEffect } from "react";
 import { Switch, Route} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Toaster } from 'react-hot-toast';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
 import Loader from "./components/Loader/Loader";
-import { Toaster } from 'react-hot-toast';
-import { useEffect } from "react";
 import { auth } from "./features/userAuthSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { getCities, getSports } from "./features/dataSlice";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(auth());
+    dispatch(getCities());
+    dispatch(getSports());
   },[]);
 
   const isLoading = useSelector((state)=>{
--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,20 +1,17 @@
-import DataProvider from "../Contexts/DataProvider";
 import User from "./User/User";
 import GuestPage from "./Guest/GuestPage";
 import { MainContainer } from "./mainStyle";
 import { useSelector } from "react-redux";
 
 const Main = (props) => {
-    const loggedIn = useSelector((state)=>{
+    const loggedIn = useSelector((state) => {
         return state.user.value.loggedIn
     });
 
     return (
-        <DataProvider>
-            <MainContainer>
-                {loggedIn ? <User/> : <GuestPage {...props}/>}
-            </MainContainer>
-        </DataProvider>
+        <MainContainer>
+            {loggedIn ? <User /> : <GuestPage {...props} />}
+        </MainContainer>
     );
 };
 
