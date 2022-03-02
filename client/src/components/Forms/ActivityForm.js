f723ec0c8ef173da79264623a44f26dda3747e08
Radostin Kirilov <radostin.kirilov@ad.mentormate.bg>
2022-03-02 17:33:04 +0200

    Major rework of the activity form. Switch to functional component instead of class and replaced the dataContext with the values from dataSlice

--- client/src/components/Forms/ActivityForm.js
+++ client/src/components/Forms/ActivityForm.js
@@ -1,44 +1,54 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
 import { Wrapper, Form, Input, Submit, Label, Select } from "./formStyle";
 import { Error } from "../Main/mainStyle";
-import toast from "react-hot-toast";
 import { fetcher } from "../../utils/helpers";
 import { validation } from "../../utils/plainText";
-import DataContext from "../Contexts/DataContext";
 
-class ActivityForm extends Component {
-    constructor(props) {
-        super(props)
+function ActivityForm(props) {
+    const [state, setState] = useState({
+        nameOfUser: "",
+        sport: "Football",
+        date: null,
+        description: "",
+        peopleNeeded: 1,
+        phoneNumber: "",
+        imgUrl: "",
+        city: "Sofia",
+        redirect: null,
+        dateFromPublication: "",
+        errors: {}
+    });
 
-        this.state = {
-            nameOfUser: "",
-            sport: "Football",
-            date: null,
-            description: "",
-            peopleNeeded: 1,
-            phoneNumber: "",
-            imgUrl: "",
-            city: "Sofia",
-            cities: [],
-            sports: [],
-            redirect: null,
-            dateFromPublication: "",
-            errors: {}
+    const cities = useSelector((state) => state.data.value.cities);
+    const sports = useSelector((state) => state.data.value.sports);
+
+    useEffect(() => {
+        if (props.id) {
+            fetch(`http://localhost:5000/api/publications/${props.id}`)
+                .then(res => res.json())
+                .then(data => {
+                    setState((prevState) => ({
+                        ...prevState,
+                        nameOfUser: data.publication.nameOfUser,
+                        date: data.publication.date,
+                        description: data.publication.description,
+                        peopleNeeded: Number(data.publication.peopleNeeded),
+                        phoneNumber: data.publication.phoneNumber,
+                        imgUrl: data.publication.imgUrl,
+                        dateFromPublication: data.publication.date
+                    }));
+                }).catch(err => console.log(err));
         };
-        this.onChangeHandler = this.onChangeHandler.bind(this);
-        this.onEditHandler = this.onEditHandler.bind(this);
-        this.onFocusHandler = this.onFocusHandler.bind(this);
-        this.onSubmitHandler = this.onSubmitHandler.bind(this);
-    };
+    }, []);
 
-    static contextType = DataContext;
-
-    validateForm() {
-        const { nameOfUser, date, description, peopleNeeded, phoneNumber, imgUrl } = this.state;
+    const validateForm = () => {
+        const { nameOfUser, date, description, peopleNeeded, phoneNumber, imgUrl } = state;
+        const message = validation.activity;
         let errors = {};
         let isValid = true;
-        let message = validation.activity;
 
         if ((nameOfUser.trim().length === 0) || /^([A-Z]{1})([a-z])+ ([A-Z]{1})([a-z])+$/.test(nameOfUser) === false) {
             errors.nameOfUser = message.nameOfUser;
@@ -68,163 +78,130 @@ class ActivityForm extends Component {
             errors.imgUrl = message.imgUrl;
         };
 
-        this.setState({
+        setState((prevState) => ({
+            ...prevState,
             errors
-        });
+        }));
 
         if (Object.keys(errors).length > 0) {
             toast.error(message.error);
             isValid = false;
         };
+
         return isValid;
     };
 
-    onFocusHandler() {
-        this.setState({
+    const onFocusHandler = () => {
+        setState((prevState) => ({
+            ...prevState,
             errors: {}
-        });
+        }));
     };
 
-    onChangeHandler(e) {
-        this.setState({
+    const onChangeHandler = (e) => {
+        setState((prevState) => ({
+            ...prevState,
             [e.target.name]: e.target.value
-        });
+        }));
     };
 
-    onSubmitHandler(e) {
+    const onSubmitHandler = (e) => {
         e.preventDefault();
-        const { nameOfUser, sport, date, description, phoneNumber, city, imgUrl } = this.state;
-        const peopleNeeded = Number(this.state.peopleNeeded);
-        const isValid = this.validateForm();
-        const endpoint = "/publications/create";
-        isValid && fetcher(endpoint, "POST", { nameOfUser, sport, date, description, phoneNumber, city, imgUrl, peopleNeeded })
+
+        const isValid = validateForm();
+        const { nameOfUser, sport, date, description, phoneNumber, city, imgUrl } = state;
+        const peopleNeeded = Number(state.peopleNeeded);
+        const endpoint = props.id ? `/publications/${props.id}` : "/publications/create";
+        const typeOfRequest = props.id ? "PATCH" : "POST";
+        const redirect = props.id ? `/details/${props.id}` : "/";
+        const toastMessage = props.id ? 'Activity successfully updated!' : 'Activity successfully created!';
+        const icon = props.id ? "👏" : "🏆";
+
+        isValid && fetcher(endpoint, typeOfRequest, { nameOfUser, sport, date, description, phoneNumber, city, imgUrl, peopleNeeded })
             .then(response => response.json())
             .then(data => {
                 if (data.message) throw data.message;
-                toast.success('Activity successfully created!',{
-                    icon:"🏆"
+                toast.success(toastMessage, {
+                    icon
                 });
                 console.log('Success:', data);
-                this.setState({
-                    redirect: "/"
-                });
-            }).catch(err => console.log(err));
-    };
-
-    onEditHandler(e) {
-        e.preventDefault();
-        let isValid = this.validateForm();
-        let endpoint = `/publications/${this.props.id}`;
-        let { nameOfUser, sport, date, description, phoneNumber, city, imgUrl } = this.state;
-        let peopleNeeded = Number(this.state.peopleNeeded);
-        isValid && fetcher(endpoint, "PATCH", { nameOfUser, sport, date, description, phoneNumber, city, imgUrl, peopleNeeded })
-            .then(response => response.json())
-            .then(data => {
-                if (data.message) throw data.message;
-                toast.success('Activity successfully updated!',{
-                    icon:"👏"
-                });
-                console.log('Success:', data);
-                this.setState({
-                    redirect: `/details/${this.props.id}`
+                setState({
+                    redirect
                 })
             }).catch(err => console.log(err));
     };
 
-    componentDidMount() {
-        this.setState({
-            cities: this.context.cities,
-            sports: this.context.sports
-        });
-
-        if (this.props.id) {
-            fetch(`http://localhost:5000/api/publications/${this.props.id}`)
-                .then(res => res.json())
-                .then(data => {
-                    this.setState({
-                        nameOfUser: data.publication.nameOfUser,
-                        date: data.publication.date,
-                        description: data.publication.description,
-                        peopleNeeded: Number(data.publication.peopleNeeded),
-                        phoneNumber: data.publication.phoneNumber,
-                        imgUrl: data.publication.imgUrl,
-                        dateFromPublication: data.publication.date
-                    })
-                }).catch(err => console.log(err));
-        };
+    if (state.redirect) {
+        return <Redirect to={state.redirect} />
     };
 
-    render() {
-        if (this.state.redirect) {
-            return <Redirect to={this.state.redirect} />
-        };
-        const fontSize = "13px";
-        let errors = this.state.errors;
-        return (
-            <Wrapper>
-                <Form onSubmit={this.props.id ? this.onEditHandler : this.onSubmitHandler}>
-                    <Label htmlFor="nameOfUser">First and last name of creator:</Label>
-                    <Input type="text" name="nameOfUser" id="nameOfUser" placeholder="Example Example"
-                        defaultValue={this.state.nameOfUser}
-                        onFocus={this.onFocusHandler}
-                        onChange={this.onChangeHandler} />
-                    {errors.nameOfUser && <Error fontSize={fontSize}>{errors.nameOfUser}</Error>}
+    const fontSize = "13px";
+    const errors = state.errors;
 
-                    <Label htmlFor="description">About the activity:</Label>
-                    <Input type="text" name="description" id="description" placeholder="exact location etc."
-                        defaultValue={this.state.description}
-                        onFocus={this.onFocusHandler}
-                        onChange={this.onChangeHandler} />
-                    {errors.descriptionChars && <Error fontSize={fontSize}>{errors.descriptionChars}</Error>}
-                    {errors.descriptionLength && <Error fontSize={fontSize}>{errors.descriptionLength}</Error>}
+    return (
+        <Wrapper>
+            <Form onSubmit={onSubmitHandler}>
+                <Label htmlFor="nameOfUser">First and last name of creator:</Label>
+                <Input type="text" name="nameOfUser" id="nameOfUser" placeholder="Example Example"
+                    defaultValue={state.nameOfUser}
+                    onFocus={onFocusHandler}
+                    onChange={onChangeHandler} />
+                {errors.nameOfUser && <Error fontSize={fontSize}>{errors.nameOfUser}</Error>}
 
-                    <Label htmlFor="date">Date:</Label>
-                    {this.props.dateInfoNeeded && <div>If you don't want to select a new date it will remain: {this.state.dateFromPublication}</div>}
-                    <Input type="datetime-local" name="date" id="date"
-                        onFocus={this.onFocusHandler}
-                        onChange={this.onChangeHandler} />
-                    {errors.date && <Error fontSize={fontSize}>{errors.date}</Error>}
+                <Label htmlFor="description">About the activity:</Label>
+                <Input type="text" name="description" id="description" placeholder="exact location etc."
+                    defaultValue={state.description}
+                    onFocus={onFocusHandler}
+                    onChange={onChangeHandler} />
+                {errors.descriptionChars && <Error fontSize={fontSize}>{errors.descriptionChars}</Error>}
+                {errors.descriptionLength && <Error fontSize={fontSize}>{errors.descriptionLength}</Error>}
 
-                    <Label htmlFor="peopleNeeded">People needed:</Label>
-                    <Input type="number" min="1" max="20" name="peopleNeeded" id="peopleNeeded"
-                        value={this.state.peopleNeeded}
-                        onFocus={this.onFocusHandler}
-                        onChange={this.onChangeHandler} />
-                    {errors.peopleNeeded && <Error fontSize={fontSize}>{errors.peopleNeeded}</Error>}
+                <Label htmlFor="date">Date:</Label>
+                {props.dateInfoNeeded && <div>If you don't want to select a new date it will remain: {state.dateFromPublication}</div>}
+                <Input type="datetime-local" name="date" id="date"
+                    onFocus={onFocusHandler}
+                    onChange={onChangeHandler} />
+                {errors.date && <Error fontSize={fontSize}>{errors.date}</Error>}
 
-                    <Label htmlFor="sport">Choose sport:</Label>
-                    <Select name="sport" id="sport"
-                        onFocus={this.onFocusHandler}
-                        onChange={this.onChangeHandler}>
-                        {this.state.sports.map((sport) => <option key={sport} value={sport}> {sport} </option>)}
-                    </Select>
+                <Label htmlFor="peopleNeeded">People needed:</Label>
+                <Input type="number" min="1" max="20" name="peopleNeeded" id="peopleNeeded"
+                    value={state.peopleNeeded}
+                    onFocus={onFocusHandler}
+                    onChange={onChangeHandler} />
+                {errors.peopleNeeded && <Error fontSize={fontSize}>{errors.peopleNeeded}</Error>}
 
-                    <Label htmlFor="city">Choose your city:</Label>
-                    <Select name="city" id="city"
-                        onFocus={this.onFocusHandler}
-                        onChange={this.onChangeHandler}>
-                        {this.state.cities.map((city) => <option key={city} value={city}> {city} </option>)}
-                    </Select>
+                <Label htmlFor="sport">Choose sport:</Label>
+                <Select name="sport" id="sport"
+                    onFocus={onFocusHandler}
+                    onChange={onChangeHandler}>
+                    {sports.map((sport) => <option key={sport} value={sport}> {sport} </option>)}
+                </Select>
 
-                    <Label htmlFor="phoneNumber">Phone number:</Label>
-                    <Input type="text" name="phoneNumber" id="phoneNumber" placeholder="0800000000"
-                        defaultValue={this.state.phoneNumber}
-                        onFocus={this.onFocusHandler}
-                        onChange={this.onChangeHandler} />
-                    {errors.phoneNumber && <Error fontSize={fontSize}>{errors.phoneNumber}</Error>}
+                <Label htmlFor="city">Choose your city:</Label>
+                <Select name="city" id="city"
+                    onFocus={onFocusHandler}
+                    onChange={onChangeHandler}>
+                    {cities.map((city) => <option key={city} value={city}> {city} </option>)}
+                </Select>
 
-                    <Label htmlFor="imgUrl">Image Url:</Label>
-                    <Input type="text" name="imgUrl" id="imgUrl" placeholder="http://example"
-                        defaultValue={this.state.imgUrl}
-                        onFocus={this.onFocusHandler}
-                        onChange={this.onChangeHandler} />
-                    {errors.imgUrl && <Error fontSize={fontSize}>{errors.imgUrl}</Error>}
+                <Label htmlFor="phoneNumber">Phone number:</Label>
+                <Input type="text" name="phoneNumber" id="phoneNumber" placeholder="0800000000"
+                    defaultValue={state.phoneNumber}
+                    onFocus={onFocusHandler}
+                    onChange={onChangeHandler} />
+                {errors.phoneNumber && <Error fontSize={fontSize}>{errors.phoneNumber}</Error>}
 
-                    <Submit className="activity-submit" value={this.props.buttonName || "Create"} />
-                </Form>
-            </Wrapper>
-        );
-    };
-};
+                <Label htmlFor="imgUrl">Image Url:</Label>
+                <Input type="text" name="imgUrl" id="imgUrl" placeholder="http://example"
+                    defaultValue={state.imgUrl}
+                    onFocus={onFocusHandler}
+                    onChange={onChangeHandler} />
+                {errors.imgUrl && <Error fontSize={fontSize}>{errors.imgUrl}</Error>}
+
+                <Submit className="activity-submit" value={props.buttonName || "Create"} />
+            </Form>
+        </Wrapper>
+    )
+}
 
 export default ActivityForm
