b80843747c000073b357644123c902b8d6fa79a7
Radostin Kirilov <radostin.kirilov@ad.mentormate.bg>
2022-03-07 17:01:34 +0200

    City stays in state after page refreshes

--- server/services/authService.js
+++ server/services/authService.js
@@ -5,29 +5,31 @@ const { LOGIN_FAIL, REGISTER_USERNAME_ALREADY_IN_USE } = require("../utils/error
 const { SECRET } = require("../config/config");
 
 async function register(username, password, city) {
-    let user = await User.findOne({ username });
+    const user = await User.findOne({ username });
 
     if (user) {
         throw new Error(REGISTER_USERNAME_ALREADY_IN_USE);
     }
 
-    let createdUser = new User({ username, password, city });
+    const createdUser = new User({ username, password, city });
     return createdUser.save();
 }
 
 async function login(username, password) {
-    let user = await User.findOne({ username });
+    const user = await User.findOne({ username });
+
     if (!user) {
         throw new Error(LOGIN_FAIL);
-    }
+    };
 
-    let isPassCorrect = await bcrypt.compare(password, user.password);
+    const isPassCorrect = await bcrypt.compare(password, user.password);
+
     if (!isPassCorrect) {
         throw new Error(LOGIN_FAIL);
-    }
+    };
 
-    let token = jwt.sign({id: user._id, username: user.username},SECRET,{expiresIn:"1h"});
-    return {id: user._id, username:user.username, city: user.city, token};
+    const token = jwt.sign({ id: user._id, username: user.username, city: user.city }, SECRET, { expiresIn:"1h" });
+    return { id: user._id, username:user.username, city: user.city, token };
 }
 
 module.exports = {
